5fd3a4a9ac10cb5ba35440bacd12c2681ea23f92
vandilsonbrito <vandilson25@outlook.com>
2023-12-13 11:18:43 -0300

    Create Footer component

---
+++ src/Web/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Footer() {
+  return (
+    <div className="w-full h-full py-1 text-white flex flex-col items-center justify-center">
+        <p className="capitalize text-xs lg:text-sm">Informações retiradas da internet (<a href="https://suits.fandom.com/wiki/Suits_Wiki">Suits.fandom</a>, etc.).</p>
+        <p className="text-xs pt-1">Desenvolvido por <a className="underline underline-offset-2" href="https://www.linkedin.com/in/vandilson-brito-b791b3216/">Vandilson Brito.</a></p>
+    </div>
+  )
+}
