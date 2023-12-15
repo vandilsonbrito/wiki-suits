3b104738d6b8141c1a92afecbf1c709d9a291b1c
vandilsonbrito <vandilson25@outlook.com>
2023-12-15 14:08:53 -0300

    Adds harvey-mike img and update styles

--- src/Web/components/Footer/Footer.jsx
+++ src/Web/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
 
+import imgHarveyMike from '../../assets/images/harvey-mike.png';
+
 
 export default function Footer() {
   return (
-    <div className="w-full h-16 py-1 text-white flex flex-col items-center justify-center gap-2">
-        <h5 className="capitalize text-xs lg:text-sm m-0">Informações retiradas da internet.</h5>
-        <h5 className="text-xs ">Desenvolvido por <a className="underline underline-offset-2" href="https://www.linkedin.com/in/vandilson-brito-b791b3216/">Vandilson Brito.</a></h5>
+    <div className="w-full h-full text-white flex flex-col items-center justify-center gap-3">
+        <div className="w-full flex justify-center">
+            <img className="w-80 md:w-96" src={imgHarveyMike} alt="" />
+        </div>
+        <div className='w-full flex flex-col items-center pb-2'>
+          <h5 className="capitalize text-xs lg:text-sm m-0">Informações retiradas da internet.</h5>
+          <h5 className="text-xs ">Desenvolvido por <a className="underline underline-offset-2" href="https://www.linkedin.com/in/vandilson-brito-b791b3216/">Vandilson Brito.</a></h5>
+        </div>
     </div>
   )
 }
