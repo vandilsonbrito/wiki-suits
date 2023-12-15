764644b91f34b6c33601f1810f0d4e3bcbb9118e
vandilsonbrito <vandilson25@outlook.com>
2023-12-15 14:05:52 -0300

    Adds styles (bg-black) to every page

--- src/Web/App.jsx
+++ src/Web/App.jsx
@@ -1,20 +1,21 @@
 import { AnimatePresence } from 'framer-motion';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Home from './pages/home/Home';
+import Home from './pages/home/Home'
 import Personagem from './pages/personagem/Personagem';
 
 function App() {
   
   const location = useLocation();
-  console.log(location)
 
   return (
 
       <AnimatePresence mode='wait'>
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Home />} key="/"/>
-            <Route path="/character/:slug" element={<Personagem />} key="/character/:slug"/>
-          </Routes>
+          <div className='w-full h-full bg-black'>
+            <Routes location={location} key={location.pathname}>
+              <Route path="/" element={<Home />} key="/"/>
+              <Route path="/character/:slug" element={<Personagem />} key="/:slug"/>
+            </Routes>
+          </div>
       </AnimatePresence>
   )
 }
